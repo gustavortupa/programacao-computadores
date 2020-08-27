6ae072a810896cedb962c21385ca1abccb208ca4
gustavortupa <gustavortm@gmail.com>
2020-08-26 21:40:47 -0300

    Exercício 07 - Completo

--- lista1-exe7.js
+++ lista1-exe7.js
@@ -1,9 +1,10 @@
 chefe()
 
-function chefe() {
+function chefe(){
     var vetor = [] // declara o vetor
     exe7(vetor) // passando o vetor como parâmetro - escopo GLOBAL
-    
+    console.log(vetor)
+    calculaMedia(vetor)
 }
 
 function exe7(vet) { //vet representa o vetor -> o que fizermos com vet, será feito com vetor
@@ -14,4 +15,14 @@ function exe7(vet) { //vet representa o vetor -> o que fizermos com vet, será f
         numero = Number(prompt("Digite outro número ou um número negativo para encerrar:"))
      }
     while(numero >= 0)
+
+}
+
+//calcula e mostra a Média
+function calculaMedia(vet){
+     var soma = 0
+     for(var i = 0; i < vet.length;i++){
+         soma = soma + vet[i]
+     }
+     console.log(`A média é ${soma/vet.length}`)  
 }
