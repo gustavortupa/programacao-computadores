5a3c04647f3c4b2f5b4ace1568d6f75368a3936e
gustavortupa <gustavortm@gmail.com>
2020-08-26 21:55:16 -0300

    Exercício 09 - Completo

--- lista1-exe9.js
+++ lista1-exe9.js
@@ -1,1 +1,16 @@
-console.log("exercício 09")
+chefe()
+
+function chefe() {
+    var numero = Number(prompt("Digite um número inteiro e positivo:"))
+    exe9(numero)
+}
+
+function exe9(num) {
+    var divisores = 0
+    for(var i = num; i > 0; i--){
+          if(num % i == 0){
+            divisores = divisores + i 
+           }
+    }
+    console.log(`A soma dos divisores é ${divisores}`)
+}
