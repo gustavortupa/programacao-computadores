c371363ba4ae6a6f6ba1da87b611e6d523a62a48
gustavortupa <gustavortm@gmail.com>
2020-08-26 21:35:26 -0300

    exercício 07 sem os cálculos

--- lista1-exe7.js
+++ lista1-exe7.js
@@ -1,1 +1,17 @@
-console.log("exercício 07")
+chefe()
+
+function chefe() {
+    var vetor = [] // declara o vetor
+    exe7(vetor) // passando o vetor como parâmetro - escopo GLOBAL
+    
+}
+
+function exe7(vet) { //vet representa o vetor -> o que fizermos com vet, será feito com vetor
+    var numero = Number(prompt("Digite um número:"))
+    do {
+        // coloca o numero em vet 
+        vet.push(numero)
+        numero = Number(prompt("Digite outro número ou um número negativo para encerrar:"))
+     }
+    while(numero >= 0)
+}
